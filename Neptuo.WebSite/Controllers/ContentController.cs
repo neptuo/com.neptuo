b779bce7bfc094cd49f407b339a8d9eef29b52e4
Marek Fišera <maraf@users.noreply.github.com>
2013-08-23 18:55:39 +0000

    Design

---
+++ Neptuo.WebSite/Controllers/ContentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Neptuo.WebSite.Controllers
+{
+    public class ContentController : Controller
+    {
+        public ActionResult Index()
+        {
+            string pathValue = (RouteData.Values["path"] ?? String.Empty).ToString();
+            if (String.IsNullOrEmpty(pathValue))
+                pathValue = "Hello";
+
+            if (!System.IO.File.Exists(Request.MapPath(Path.Combine("~/Views/Content", pathValue + ".cshtml"))))
+                pathValue = "NotFound";
+
+            return View(pathValue);
+        }
+
+    }
+}
