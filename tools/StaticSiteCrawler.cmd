78824b72ce7ff8bde126c2e876e973dd04b9cd27
maraf <mara@neptuo.com>
2016-10-01 09:05:08 +0200

    Add pause on the of the crawler cmd.

--- tools/StaticSiteCrawler.cmd
+++ tools/StaticSiteCrawler.cmd
@@ -2,3 +2,5 @@ StaticSiteCrawler.exe http://localhost:61057/ ..\artifacts\StaticSite / /404.htm
 
 xcopy ..\Neptuo.WebSite\Content ..\artifacts\StaticSite\Content /E /R /Y
 xcopy ..\Neptuo.WebSite\Scripts ..\artifacts\StaticSite\Scripts /E /R /Y
+
+pause
