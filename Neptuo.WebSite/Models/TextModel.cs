656150b214144fb03dd005fdb350c35ba03920d8
maraf <mara@neptuo.com>
2016-10-11 12:33:24 +0200

    Add support for Text with Language.

---
+++ Neptuo.WebSite/Models/TextModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace Neptuo.WebSite.Models
+{
+    public class TextModel
+    {
+        [XmlAttribute]
+        public Language Language { get; set; }
+
+        [XmlText]
+        public string Content { get; set; }
+
+        public string ContentToHtml()
+        {
+            switch (Language)
+            {
+                case Language.PlainText:
+                case Language.Html:
+                    return Content;
+                case Language.Markdown:
+                    return CommonMarkConverter.Convert(Content);
+                default:
+                    throw new NotSupportedException(Language.ToString());
+            }
+        }
+    }
+
+    public enum Language
+    {
+        PlainText,
+        Markdown,
+        Html
+    }
+}
