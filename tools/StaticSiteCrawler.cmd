255cb8ac0faebbde7d4c311e1e834de6109c5372
maraf <mara@neptuo.com>
2016-10-21 21:51:58 +0200

    Remove year from blog post list. Update crawler to new port.

--- tools/StaticSiteCrawler.cmd
+++ tools/StaticSiteCrawler.cmd
@@ -1,6 +1,6 @@
-StaticSiteCrawler.exe http://localhost:61057/ ..\artifacts\StaticSite / /404.html
+StaticSiteCrawler.exe http://localhost:7287/ ..\artifacts\StaticSite / /404.html
 
-xcopy ..\Neptuo.WebSite\Content ..\artifacts\StaticSite\Content /E /R /Y
-xcopy ..\Neptuo.WebSite\Scripts ..\artifacts\StaticSite\Scripts /E /R /Y
+xcopy ..\src\Neptuo.WebSite\Content ..\artifacts\StaticSite\Content /E /R /Y
+xcopy ..\src\Neptuo.WebSite\Scripts ..\artifacts\StaticSite\Scripts /E /R /Y
 
 pause
