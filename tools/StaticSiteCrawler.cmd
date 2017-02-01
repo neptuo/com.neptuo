ede33698e4a753b6583669f9af1147aec0bdd6bf
Marek Fišera <mara@neptuo.com>
2017-02-01 19:55:13 +0100

    Update StaticSiteCrawler.cmd to copy only release content of Content folder.

--- tools/StaticSiteCrawler.cmd
+++ tools/StaticSiteCrawler.cmd
@@ -1,6 +1,9 @@
 StaticSiteCrawler.exe http://localhost:7287/ ..\artifacts\StaticSite / /404.html
 
-xcopy ..\src\Neptuo.WebSite\Content ..\artifacts\StaticSite\Content /E /R /Y
+xcopy ..\src\Neptuo.WebSite\Content\Images ..\artifacts\StaticSite\Content\Images /E /R /Y
+xcopy ..\src\Neptuo.WebSite\Content\bootstrap.min.css ..\artifacts\StaticSite\Content\bootstrap.min.css /R /Y
+xcopy ..\src\Neptuo.WebSite\Content\bootstrap-theme.min.css ..\artifacts\StaticSite\Content\bootstrap-theme.min.css /R /Y
+xcopy ..\src\Neptuo.WebSite\Content\web.css ..\artifacts\StaticSite\Content\web.css /R /Y
 xcopy ..\src\Neptuo.WebSite\Scripts ..\artifacts\StaticSite\Scripts /E /R /Y
 
 pause
