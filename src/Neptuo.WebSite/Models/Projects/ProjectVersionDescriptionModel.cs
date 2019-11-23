a59313852e0b82b69b40881be295c2ac1ecdec55
Marek Fišera <mara@neptuo.com>
2019-11-23 18:06:15 +0100

    Render links to github issues.

--- src/Neptuo.WebSite/Models/Projects/ProjectVersionDescriptionModel.cs
+++ src/Neptuo.WebSite/Models/Projects/ProjectVersionDescriptionModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Xml.Serialization;
 
@@ -9,6 +10,8 @@ namespace Neptuo.WebSite.Models.Projects
     [XmlType("Description")]
     public class ProjectVersionDescriptionModel
     {
+        private static Regex issueRegex = new Regex("(#([0-9])+)", RegexOptions.Compiled);
+
         [XmlText]
         public string Content { get; set; }
 
@@ -18,6 +21,14 @@ namespace Neptuo.WebSite.Models.Projects
         public ProjectVersionDescriptionModel()
         {
             Descriptions = new List<ProjectVersionDescriptionModel>();
+        }
+
+        public string GetContent(string projectUrl)
+        {
+            if (projectUrl.Contains("github.com"))
+                return issueRegex.Replace(Content, match => $"<a target='_blank' href='{projectUrl}/issues/{match.Groups[2].Value}'>{match.Groups[0].Value}</a>");
+
+            return Content;
         }
     }
 }
